c254f25fb64241ce0a9c98b93247e88b07368d18
cruellaDev <cruellaDevLee@gmail.com>
2024-05-18 22:37:07 +0900

    add

---
+++ src/main/java/com/study/tdd/requirements/PrimeFactors.java
@@ -0,0 +1,4 @@
+package com.study.tdd.requirements;
+
+public class PrimeFactors {
+}
---
+++ src/test/java/com/study/tdd/requirements/TennisTest.java
@@ -0,0 +1,30 @@
+package com.study.tdd.requirements;
+
+public class TennisTest {
+    // TODO - set ready
+    // TODO - -1점 WITHDRAW
+    // TODO - 0점 Love
+    // TODO - +1점 A FIFTEEN
+    // TODO - +1점 A THIRTY
+    // TODO - +1점 A FORTY
+    // TODO - +1점 A wins
+    // TODO - +1점 B FIFTEEN
+    // TODO - +1점 B THIRTY
+    // TODO - +1점 B FORTY
+    // TODO - +1점 B wins
+    // TODO - set end
+    // TODO - set ready
+    // TODO - 0점 Love
+    // TODO - +1점 A FIFTEEN
+    // TODO - +1점 A THIRTY
+    // TODO - +1점 A FORTY
+    // TODO - +1점 B FIFTEEN
+    // TODO - +1점 B THIRTY
+    // TODO - +1점 B FORTY
+    // TODO - +1점 B deuce
+    // TODO - +1점 B deuce, advantage
+    // TODO - +1점 A deuce
+    // TODO - +1점 A deuce, advantage
+    // TODO - +1점 A wins
+    // TODO - set end
+}
