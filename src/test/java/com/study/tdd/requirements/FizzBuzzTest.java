917ee0aa556a922012a55bcd2c41d9b12f124b99
cruellaDev <cruellaDevLee@gmail.com>
2024-05-18 18:25:30 +0900

    refactoring

--- src/test/java/com/study/tdd/requirements/FizzBuzzTest.java
+++ src/test/java/com/study/tdd/requirements/FizzBuzzTest.java
@@ -21,76 +21,81 @@ public class FizzBuzzTest {
         return number % 3 == 0 && number % 5 == 0;
     }
 
-    private void assertFizzBuzzPass(int number, String expStr, boolean condition) {
+    private boolean isVerifiedStrByNum(int number, String str) {
+        return switch (str) {
+            case "Fizz" -> isFizz(number);
+            case "Buzz" -> isBuzz(number);
+            case "FizzBuzz" -> isFizzBuzz(number);
+            default -> str.equals(String.valueOf(number));
+        };
+    }
+
+    private void assertFizzBuzzPass(int number, String expStr) {
         assertEquals(expStr, FizzBuzz.answer(number));
+        boolean condition = isVerifiedStrByNum(number, expStr);
         assertTrue(condition);
     }
 
-    private void assertFizzBuzzFail(int number, String expStr, boolean condition) {
+    private void assertFizzBuzzFail(int number, String expStr) {
         assertNotEquals(expStr, FizzBuzz.answer(number));
+        boolean condition = isVerifiedStrByNum(number, expStr);
         assertFalse(condition);
     }
 
     @Test
     void testIfFizzPass() {
-        assertFizzBuzzPass(3, "Fizz", isFizz(3));
-        assertFizzBuzzPass(57, "Fizz", isFizz(57));
-        assertFizzBuzzPass(99, "Fizz", isFizz(99));
+        assertFizzBuzzPass(3, "Fizz");
+        assertFizzBuzzPass(57, "Fizz");
+        assertFizzBuzzPass(99, "Fizz");
     }
 
     @Test
     void testIfFizzFail() {
-        assertFizzBuzzFail(20, "Fizz", isFizz(20));
-        assertFizzBuzzFail(45, "Fizz", isFizz(45));
-        assertFizzBuzzFail(88, "Fizz", isFizz(88));
+        assertFizzBuzzFail(20, "Fizz");
+        assertFizzBuzzFail(45, "Fizz");
+        assertFizzBuzzFail(88, "Fizz");
     }
 
     @Test
     void testIfBuzzPass() {
-        assertFizzBuzzPass(5, "Buzz", isBuzz(5));
-        assertFizzBuzzPass(10, "Buzz", isBuzz(10));
-        assertFizzBuzzPass(100, "Buzz", isBuzz(100));
+        assertFizzBuzzPass(5, "Buzz");
+        assertFizzBuzzPass(10, "Buzz");
+        assertFizzBuzzPass(100, "Buzz");
     }
 
     @Test
     void testIfBuzzFail() {
-        assertFizzBuzzFail(81, "Buzz", isBuzz(81));
-        assertFizzBuzzFail(75, "Buzz", isBuzz(75));
-        assertFizzBuzzFail(29, "Buzz", isBuzz(29));
+        assertFizzBuzzFail(81, "Buzz");
+        assertFizzBuzzFail(75, "Buzz");
+        assertFizzBuzzFail(29, "Buzz");
     }
 
     @Test
     void testIfFizzBuzzPass() {
-        assertFizzBuzzPass(75, "FizzBuzz", isFizzBuzz(75));
-        assertFizzBuzzPass(90, "FizzBuzz", isFizzBuzz(90));
-        assertFizzBuzzPass(15, "FizzBuzz", isFizzBuzz(15));
+        assertFizzBuzzPass(75, "FizzBuzz");
+        assertFizzBuzzPass(90, "FizzBuzz");
+        assertFizzBuzzPass(15, "FizzBuzz");
     }
 
     @Test
     void testIfFizzBuzzFail() {
-        assertFizzBuzzFail(23, "FizzBuzz", isFizzBuzz(23));
-        assertFizzBuzzFail(84, "FizzBuzz", isFizzBuzz(84));
-        assertFizzBuzzFail(35, "FizzBuzz", isFizzBuzz(35));
+        assertFizzBuzzFail(23, "FizzBuzz");
+        assertFizzBuzzFail(84, "FizzBuzz");
+        assertFizzBuzzFail(35, "FizzBuzz");
     }
 
     @Test
     void testIfFizzBuzzValid() {
         String actual;
-        boolean isValid = false;
+        boolean isVerified = false;
 
         for (int i = 1; i <= 100; i++) {
             actual = FizzBuzz.answer(i);
+            isVerified = isVerifiedStrByNum(i, actual);
 
-            isValid = switch (actual) {
-                case "Fizz" -> isFizz(i);
-                case "Buzz" -> isBuzz(i);
-                case "FizzBuzz" -> isFizzBuzz(i);
-                default -> actual.equals(String.valueOf(i));
-            };
-
-            if (!isValid) throw new AssertionError(i);
+            if (!isVerified) throw new AssertionError(i);
         }
 
-        assertTrue(isValid);
+        assertTrue(isVerified);
     }
 }
