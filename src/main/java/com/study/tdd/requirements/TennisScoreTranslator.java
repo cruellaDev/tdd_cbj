d727d2b004faac455bb04aae17f2f15700c42b32
cruellaDev <cruellaDevLee@gmail.com>
2024-05-22 22:17:33 +0900

    .

--- src/main/java/com/study/tdd/requirements/TennisScoreTranslator.java
+++ src/main/java/com/study/tdd/requirements/TennisScoreTranslator.java
@@ -1,22 +1,47 @@
 package com.study.tdd.requirements;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TennisScoreTranslator {
     public String showScores(TennisGame tennisGame) {
-        String player1Name = tennisGame.getPlayer1Name();
-        String player2Name = tennisGame.getPlayer2Name();
-        int player1Points = tennisGame.getPlayer1Points();
-        int player2Points = tennisGame.getPlayer2Points();
-        int maxPoints = Math.max(player1Points, player2Points);
-        int pointsDiff = Math.abs(player1Points - player2Points);
-        if (maxPoints >= 3 && pointsDiff == 0) {
-            return TennisGameScores.DEUCE.name();
-        } else if (maxPoints > 3 && pointsDiff == 1) {
-            return (player1Points > player2Points ? player1Name : player2Name) + " : " + TennisGameScores.ADVANTAGE;
-        } else if (maxPoints > 3 && pointsDiff > 1) {
-            return (player1Points > player2Points ? player1Name : player2Name) + " : " + TennisGameScores.WINNER;
+        Map<String, TennisGameScores> matchMap = analyzeMatch(
+                tennisGame.getPlayer1Name(), tennisGame.getPlayer1Points(),
+                tennisGame.getPlayer2Name(), tennisGame.getPlayer2Points());
+        List<String> scoresScript = new ArrayList<>();
+        if (matchMap.isEmpty()) {
+            scoresScript.add(TennisGameScores.DEUCE.name());
         }
-        return player1Name + " : " + switchPointsToGameScores(player1Points)
-                + ", " + player2Name + " : " + switchPointsToGameScores(player2Points);
+        if (matchMap.containsKey(tennisGame.getPlayer1Name())) {
+            scoresScript.add(tennisGame.getPlayer1Name() + " : " + matchMap.get(tennisGame.getPlayer1Name()));
+        }
+        if (matchMap.containsKey(tennisGame.getPlayer2Name())) {
+            scoresScript.add(tennisGame.getPlayer2Name() + " : " + matchMap.get(tennisGame.getPlayer2Name()));
+        }
+        return String.join(", ", scoresScript);
+    }
+
+    private Map<String, TennisGameScores> analyzeMatch(String player1Name, int player1Points, String player2Name, int player2Points) {
+        String defender = player1Points > player2Points ? player1Name : player2Name;
+        String chaser = player1Points > player2Points ? player2Name : player1Name;
+        int maxPoints = Math.max(player1Points, player2Points);
+        int minPoints = Math.min(player1Points, player2Points);
+        int pointsDiff = Math.abs(player1Points - player2Points);
+        Map<String, TennisGameScores> matchMap = new HashMap<>();
+        if (maxPoints >= 3 && pointsDiff == 0) {
+            // matchMap is empty when Deuce
+        } else if (maxPoints > 3 && pointsDiff == 1) {
+            matchMap.put(defender, TennisGameScores.ADVANTAGE);
+        } else if (maxPoints > 3 && pointsDiff > 1) {
+            matchMap.put(defender, TennisGameScores.WINNER);
+        } else {
+            matchMap.put(defender, switchPointsToGameScores(maxPoints));
+            matchMap.put(chaser, switchPointsToGameScores(minPoints));
+        }
+
+        return matchMap;
     }
 
     private TennisGameScores switchPointsToGameScores(int gamePoints) {
